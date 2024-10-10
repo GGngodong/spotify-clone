97aa228772579875b426142b48d5005da9517cf2
GGngodong <ikkoy123@gmail.com>
2024-10-10 20:10:34 +0700

    add get started page, and choose mode image, fixing the font format at pubsec.yaml, add new configs for images

---
+++ lib/core/configs/assets/apps_images.dart
@@ -0,0 +1,6 @@
+class AppsImages {
+  static const String basePath = 'assets/images/';
+  static const String introBackground = '${basePath}intro_background.png';
+  static const String chooseModeBackground =
+      '${basePath}choose_mode_background.png';
+}
---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
+import 'package:spotify_clone/core/configs/assets/apps_images.dart';
+import 'package:spotify_clone/core/configs/assets/apps_vector.dart';
+import 'package:spotify_clone/core/configs/theme/app_colors.dart';
+import 'package:spotify_clone/presentation/mode/pages/chose_mode.dart';
+
+class GetStartedPages extends StatelessWidget {
+  const GetStartedPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AppsImages.introBackground),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 40,
+              vertical: 40,
+            ),
+            child: Column(
+              children: [
+                Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(AppsVector.logo)),
+                const Spacer(),
+                const Text(
+                  'Enjoy Listening Music',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
+                const SizedBox(height: 21),
+                const Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.grey,
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                BasicAppButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              const ChoseModePage(),
+                        ),
+                      );
+                    },
+                    title: 'Get Started'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
